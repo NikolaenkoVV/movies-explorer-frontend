82a4a19e77c3152d240c4eed295f6f7b5bd3e25c
NikolaenkoVV <korbendallas.yap@gmail.com>
2023-10-29 15:45:25 +0300

    fix: исправить url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const MOVIES_SERVER_URL = "https://api.nomoreparties.co";
-const SERVER_URL = "http://localhost:3001";
-//const SERVER_URL = "https://api.moviedallas.nomoredomainsrocks.ru"
+// const SERVER_URL = "http://localhost:3001";
+const SERVER_URL = "https://api.moviedallas.nomoredomainsrocks.ru"
 const EMPTY_QUERY_ERROR = "Нужно ввести ключевое слово";
 const SHORT_DURATION = 40;
 const SEARCH_LOCAL_STORAGE_KEY = "searchFilms";
