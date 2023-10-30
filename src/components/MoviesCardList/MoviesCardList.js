590f96f50a5a70abf185e8694f7a60d83b01bb5d
NikolaenkoVV <korbendallas.yap@gmail.com>
2023-10-30 11:45:16 +0300

    fix: убрать "магические" значения

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,6 +1,18 @@
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import { useEffect, useState } from "react";
+import {
+  MAX_MOBILE_SCREEN_DISPLAY,
+  TABLET_SCREEN_DISPLAY,
+  DESKTOP_SCREEN_DISPLAY,
+  MIN_ADDING_STEP,
+  MIDDLE_ADDING_STEP,
+  MAX_ADDING_STEP,
+  MOBILE_SCREEN_INITIAL_MOVIES_COUNT,
+  TABLET_SCREEN_INITIAL_MOVIES_COUNT,
+  DESKTOP_SCREEN_INITIAL_MOVIES_COUNT,
+  XL_DISPLAY_SCREEN_INITIAL_MOVIES_COUNT,
+} from "../../utils/constants";
 
 function MoviesCardList({
   movies,
@@ -19,23 +31,23 @@ function MoviesCardList({
       return;
     }
 
-    if (width < 630) {
-      setShowedMovies(5);
-      setAddingMovies(2);
-    } else if (width < 990) {
-      setShowedMovies(8);
-      setAddingMovies(2);
-    } else if (width < 1280) {
-      setShowedMovies(12);
-      setAddingMovies(3);
+    if (width < MAX_MOBILE_SCREEN_DISPLAY) {
+      setShowedMovies(MOBILE_SCREEN_INITIAL_MOVIES_COUNT);
+      setAddingMovies(MIN_ADDING_STEP);
+    } else if (width < TABLET_SCREEN_DISPLAY) {
+      setShowedMovies(TABLET_SCREEN_INITIAL_MOVIES_COUNT);
+      setAddingMovies(MIN_ADDING_STEP);
+    } else if (width < DESKTOP_SCREEN_DISPLAY) {
+      setShowedMovies(DESKTOP_SCREEN_INITIAL_MOVIES_COUNT);
+      setAddingMovies(MIDDLE_ADDING_STEP);
     } else {
-      setShowedMovies(16);
-      setAddingMovies(4);
+      setShowedMovies(XL_DISPLAY_SCREEN_INITIAL_MOVIES_COUNT);
+      setAddingMovies(MAX_ADDING_STEP);
     }
   };
 
   const addingMoreMovies = () => {
-    setShowedMovies(showedMovies => showedMovies + addingMovies);
+    setShowedMovies((showedMovies) => showedMovies + addingMovies);
   };
 
   useEffect(() => {
